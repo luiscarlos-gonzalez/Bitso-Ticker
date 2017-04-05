9a657ba039d54369052b2e609c313f5d1b6c8ac2
Luis Carlos González Hernández <lcarlosglez@gmail.com>
2017-04-05 18:43:54 -0500

    Initial Commit

---
+++ Bitso Ticker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bitso Ticker
+//
+//  Created by Luis Carlos  on 05/04/17.
+//  Copyright © 2017 Kentverger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bitso Ticker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bitso Ticker
+//
+//  Created by Luis Carlos  on 05/04/17.
+//  Copyright © 2017 Kentverger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
